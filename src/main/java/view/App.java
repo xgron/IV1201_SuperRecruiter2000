d1b4c550a722bdfe58e4a9c00bbb30606f8a7347
Tiberians <andsj@kth.se>
2018-01-29 10:41:56 +0100

    Testing with project

---
+++ src/main/java/view/App.java
@@ -0,0 +1,12 @@
+package view;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+/**
+ * Created by enfet on 2018-01-29.
+ */
+
+@ApplicationPath("api")
+public class App extends Application {
+}
