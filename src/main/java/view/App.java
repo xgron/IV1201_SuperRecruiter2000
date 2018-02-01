f23d5640817ecf5095f53611edd710c7048965f9
Tiberians <andsj@kth.se>
2018-02-01 14:54:03 +0100

    Added comments to View

--- src/main/java/view/App.java
+++ src/main/java/view/App.java
@@ -4,7 +4,14 @@ import javax.ws.rs.ApplicationPath;
 import javax.ws.rs.core.Application;
 
 /**
- * Created by enfet on 2018-01-29.
+ * <h1>REST API for recruitment Application</h1>
+ * This application features log in and log out
+ * functionality while being able to view
+ * users and accept applications
+ *
+ *
+ * @version 1.0
+ * @since   2018-01-25
  */
 
 @ApplicationPath("api")
