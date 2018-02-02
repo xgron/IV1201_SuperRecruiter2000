1bc5e679230dd64f13f274b76c56410d34098d51
Niclas Fölster <niclasfolster@n152-p169.eduroam.kth.se>
2018-02-02 14:41:05 +0100

    RegisterApplication complete

--- src/main/java/controller/HomeController.java
+++ src/main/java/controller/HomeController.java
@@ -2,6 +2,7 @@ package controller;
 
 
 import org.springframework.stereotype.Controller;
+import shared.ApplicationDTO;
 import shared.PersonDTO;
 
 @Controller
@@ -9,7 +10,7 @@ public class HomeController {
     public model.Model model = new model.Model();
 
     /**
-     *  This method is called from the View layer when a someone is trying to register a new user.
+     *  This method is called from the View layer when someone is trying to register a new user.
      *
      *  Calls the registerUser method from the Model object modelObject from the Model layer.
      *  Returns a PersonDTO object.
@@ -21,6 +22,15 @@ public class HomeController {
         PersonDTO user = model.user.registerUser(person);
         return user;
     }
+    /**
+     *  This method is called from the View layer when a user is trying to register their job application.
+     *
+     *  Calls the registerApplication method from the Model layer.
+     *  Returns a boolean. true if the application was valid and was successfully stored in the database. false otherwise.
+     *
+     * @param   application  An ApplicationDTO(Person Data Transfer Object), which contains all necessary data for an application.
+     * @return  check    boolean, false if error.
+     */
     public boolean registerApplication(ApplicationDTO application){
             boolean check = model.application.registerApplication(application);
         return check;
