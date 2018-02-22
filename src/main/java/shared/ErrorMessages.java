a6613132a21691a1759a9b1958343cee526714d8
jmarken <jmarken@kth.se>
2018-02-22 10:54:03 +0100

    css and errormessages

---
+++ src/main/java/shared/ErrorMessages.java
@@ -0,0 +1,21 @@
+package shared;
+
+public enum ErrorMessages {
+    EXAMPLE_ERROR_MESSAGE("This is the example text")
+    ;
+
+
+    private String errorMessage;
+
+    ErrorMessages(String errorMessage) {
+        this.errorMessage = errorMessage;
+    }
+
+    public String getErrorMessage() {
+        return errorMessage;
+    }
+
+    public void setErrorMessage(String errorMessage) {
+        this.errorMessage = errorMessage;
+    }
+}
