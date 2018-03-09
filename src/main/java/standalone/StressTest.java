f49853bcfef55f0832a93ace776ff2a29506f7c9
Joakim Markén <jmarken@kth.se>
2018-03-09 12:15:23 +0100

    testing fixed

--- src/main/java/standalone/StressTest.java
+++ src/main/java/standalone/StressTest.java
@@ -2,13 +2,13 @@ package standalone;
 
 public class StressTest {
     public static void main(String args[]) {
-        StressThread T1 = new StressThread( "StressThread-1");
+        StressThread T1 = new StressThread( "Thread_1");
         T1.start();
 
-        StressThread T2 = new StressThread( "StressThread-2");
+        StressThread T2 = new StressThread( "Thread_2");
         T2.start();
 
-        StressThread T3 = new StressThread( "StressThread-3");
+        StressThread T3 = new StressThread( "Thread_3");
         T3.start();
     }
 }
